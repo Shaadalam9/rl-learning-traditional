da5df519abf82dbd876f1607f8ae5ff854f357bf
MD SHADAB ALAM <md_shadab_alam@outlook.com>
2024-03-16 13:09:36 +0100

    .

--- src/docker.sh
+++
@@ -1,2 +0,0 @@
-docker build . -f Dockerfile -t abhilashiit/dqn_asv:2.0
-#docker push abhilashiit/dqn_asv:2.0
--- src/run_docker.sh
+++
@@ -1,7 +0,0 @@
-docker run -it --rm --gpus all --name dqn_asv \
-    --net=host \
-    --env="DISPLAY" \
-    --workdir="/home/docker/DDPG-ASV-path-follow/src" \
-    --volume=$(pwd):"/home/docker/DDPG-ASV-path-follow/src" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    abhilashiit/dqn_asv:2.0 bash
